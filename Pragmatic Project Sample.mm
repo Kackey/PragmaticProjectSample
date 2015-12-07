639da9d7b8d14d82b647427be1fc6122ad05ff64
Hidekazu Kakinuma <hidekazu.kakinuma@rakuten.com>
2015-12-07 16:21:54 +0900

    no message

---
+++ Pragmatic Project Sample.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1449154035575" ID="ID_680759103" MODIFIED="1449154048935" TEXT="Pragmatic Project Sample"/>
+</map>
